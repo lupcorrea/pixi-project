7d3a5ea363447b54967859bbb539cfce34b8c17c
Lucas Corrêa <lupcorrea@McDonalds.local>
2019-01-24 17:35:48 -0300

    First task complete!

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ var SCREEN_HEIGHT = window.innerHeight;
 // Card constants
 // The dimension proportion are an approximation of B8 format
 // according to ISO 216, used by poker cards.
-var CARD_IMAGE = "./img/random-image.png";
+var CARD_IMAGE = "./img/le-catitto.png";
 var CARD_STACK_SIZE = 144;
 var CARD_HEIGHT = SCREEN_HEIGHT / 4;
 var CARD_WIDTH = 0.775 * CARD_HEIGHT;
@@ -15,19 +15,30 @@ var Resources = PIXI.loader.resources;
 var Sprite = PIXI.Sprite;
 // Class for a card
 var Card = /** @class */ (function () {
-    function Card(stackPosition) {
+    function Card(pos) {
+        var _this = this;
         this.sprite = new Sprite();
+        this.frameCounter = 0;
+        this.startMoving = function () {
+            setTimeout(_this.move, 1000 * _this.stackPosition);
+        };
+        this.move = function () {
+            if (_this.frameCounter < 120)
+                requestAnimationFrame(_this.move);
+            _this.sprite.x += 4;
+            _this.frameCounter++;
+            //if (this.frameCounter == 120) this.sprite.y = (0.25 * this.stackPosition) + (SCREEN_HEIGHT / 2);
+        };
         app.stage.addChild(this.sprite);
         this.sprite.texture = Resources[CARD_IMAGE].texture;
+        this.stackPosition = pos;
         this.sprite.anchor.x = 0.5;
         this.sprite.anchor.y = 0.5;
-        this.sprite.x = (0.015 * stackPosition) + (SCREEN_WIDTH / 4);
-        this.sprite.y = (-0.25 * stackPosition) + (SCREEN_HEIGHT / 2);
+        this.sprite.x = (0.015 * this.stackPosition) + (SCREEN_WIDTH / 4);
+        this.sprite.y = (-0.25 * this.stackPosition) + (SCREEN_HEIGHT / 2);
         this.sprite.scale.x = CARD_WIDTH / Resources[CARD_IMAGE].texture.width;
         this.sprite.scale.y = CARD_HEIGHT / Resources[CARD_IMAGE].texture.height;
     }
-    Card.prototype.move = function () {
-    };
     return Card;
 }());
 // Canvas drawing
@@ -45,5 +56,6 @@ function setup() {
     var cardStack = [];
     for (var cardID = 0; cardID < CARD_STACK_SIZE; cardID++) {
         cardStack.push(new Card(cardID));
+        cardStack[cardID].startMoving();
     }
 }
--- ts/index.ts
+++ ts/index.ts
@@ -18,18 +18,30 @@ let Sprite = PIXI.Sprite;
 // Class for a card
 class Card {
     private sprite = new Sprite();
+    private frameCounter = 0;
+    private stackPosition: number;
 
-    move(): void {
-
+    startMoving = () => {
+        setTimeout (this.move, 1000 * this.stackPosition);
     }
 
-    constructor (stackPosition: number) {
+    move = () => {
+        if (this.frameCounter < 120) requestAnimationFrame (this.move);
+
+        this.sprite.x += 4;
+        this.frameCounter++;
+        //if (this.frameCounter == 120) this.sprite.y = (0.25 * this.stackPosition) + (SCREEN_HEIGHT / 2);
+    }
+
+    constructor (pos: number) {
         app.stage.addChild (this.sprite);
         this.sprite.texture = Resources [CARD_IMAGE].texture;
+        this.stackPosition = pos;
+
         this.sprite.anchor.x = 0.5;
         this.sprite.anchor.y = 0.5;
-        this.sprite.x = (0.015 * stackPosition) + (SCREEN_WIDTH / 4);
-        this.sprite.y = (-0.25 * stackPosition) + (SCREEN_HEIGHT / 2);
+        this.sprite.x = (0.015 * this.stackPosition) + (SCREEN_WIDTH / 4);
+        this.sprite.y = (-0.25 * this.stackPosition) + (SCREEN_HEIGHT / 2);
         this.sprite.scale.x = CARD_WIDTH / Resources [CARD_IMAGE].texture.width;
         this.sprite.scale.y = CARD_HEIGHT / Resources [CARD_IMAGE].texture.height;
     }
@@ -52,5 +64,6 @@ function setup() {
     let cardStack: Array<Card> = [];
     for (var cardID = 0; cardID < CARD_STACK_SIZE; cardID++) {
         cardStack.push (new Card (cardID));
+        cardStack [cardID].startMoving();
     }
 }
